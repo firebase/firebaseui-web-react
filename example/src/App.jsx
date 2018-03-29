2e8843bfbd31108abf9a9974585c5492eb1c9a5b
Nicolas Garnier <nivco@google.com>
2018-03-29 20:07:48 +0200

    Switching the sample to use Email/Password rather than the Facebook sign-in provider since that's easier to setup.

--- example/src/App.jsx
+++ example/src/App.jsx
@@ -41,7 +41,7 @@ class App extends React.Component {
     signInFlow: 'popup',
     signInOptions: [
       firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-      firebase.auth.FacebookAuthProvider.PROVIDER_ID,
+      firebase.auth.EmailAuthProvider.PROVIDER_ID,
     ],
     callbacks: {
       signInSuccess: () => false,
